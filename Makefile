eefb4a1659bdeea2783be2fe16116101909c6dc8
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2025-03-28 15:50:52 +0100

    Don't generate the html database

--- Makefile
+++ Makefile
@@ -3,14 +3,11 @@
 
 .PHONY: validate
 
-all: validate docs/index.html docs/cc-graph-data.js docs/cc-simple-list.json
+all: validate docs/cc-graph-data.js docs/cc-simple-list.json
 
 validate: cc-database.yaml
 	python3 ccdb_parser.py $<
 	./validate-db-version.sh
-
-docs/index.html: cc-database.yaml ccdb_parser.py
-	python3 ccdb_parser.py --quiet --format html $< > $@
 
 docs/cc-graph-data.js: cc-database.yaml ccdb_parser.py
 	python3 ccdb_parser.py --quiet --format json \
--- ccdb_parser.py
+++ ccdb_parser.py
@@ -7,39 +7,15 @@ from pathlib import Path
 from datetime import datetime
 
 import validation
-from validation import CCType, Relation, Glosses, GlossItem, Example, error
+from validation import CCType, Relation, Glosses, Example, error
 
 
 # A parsed definition is a list of either strings or links (as pairs of strings)
 ParsedDefinition = list[str | tuple[str, str]]
 
 # The different formats that we can export the database to
-OutputFormats = ['html', 'json', 'fnbr', 'graph']
+OutputFormats = ['json', 'fnbr']
 
-# The URL to the visualization graph, and symbol to show
-GraphURL = "cc-graph.html"
-GraphSymbolUse = """
-<svg class="graphSymbol" viewBox="-5 -5 65 65">
-<use href="#gvSymbol"/>
-</svg>
-"""
-GraphSymbolSVG = """
-<svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" viewBox="-5 -5 65 65">
-<defs>
-<path id="gvSymbol" d="M49,0c-3.309,0-6,2.691-6,6c0,1.035,0.263,2.009,0.726,2.86l-9.829,9.829C32.542,17.634,30.846,17,29,17
-    s-3.542,0.634-4.898,1.688l-7.669-7.669C16.785,10.424,17,9.74,17,9c0-2.206-1.794-4-4-4S9,6.794,9,9s1.794,4,4,4
-	c0.74,0,1.424-0.215,2.019-0.567l7.669,7.669C21.634,21.458,21,23.154,21,25s0.634,3.542,1.688,4.897L10.024,42.562
-	C8.958,41.595,7.549,41,6,41c-3.309,0-6,2.691-6,6s2.691,6,6,6s6-2.691,6-6c0-1.035-0.263-2.009-0.726-2.86l12.829-12.829
-	c1.106,0.86,2.44,1.436,3.898,1.619v10.16c-2.833,0.478-5,2.942-5,5.91c0,3.309,2.691,6,6,6s6-2.691,6-6c0-2.967-2.167-5.431-5-5.91
-	v-10.16c1.458-0.183,2.792-0.759,3.898-1.619l7.669,7.669C41.215,39.576,41,40.26,41,41c0,2.206,1.794,4,4,4s4-1.794,4-4
-	s-1.794-4-4-4c-0.74,0-1.424,0.215-2.019,0.567l-7.669-7.669C36.366,28.542,37,26.846,37,25s-0.634-3.542-1.688-4.897l9.665-9.665
-	C46.042,11.405,47.451,12,49,12c3.309,0,6-2.691,6-6S52.309,0,49,0z M11,9c0-1.103,0.897-2,2-2s2,0.897,2,2s-0.897,2-2,2
-	S11,10.103,11,9z M6,51c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S8.206,51,6,51z M33,49c0,2.206-1.794,4-4,4s-4-1.794-4-4
-	s1.794-4,4-4S33,46.794,33,49z M29,31c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S32.309,31,29,31z M47,41c0,1.103-0.897,2-2,2
-	s-2-0.897-2-2s0.897-2,2-2S47,39.897,47,41z M49,10c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S51.206,10,49,10z"/>
-</defs>
-</svg>
-"""
 
 class CCDB:
     glosses: Glosses
@@ -71,9 +47,7 @@ class CCDB:
 
     def export(self, args: argparse.Namespace) -> None:
         format = args.format
-        if format == "html":
-            self.print_html()
-        elif format == "json":
+        if format == "json":
             self.export_to_json(args)
         elif format == "fnbr":
             self.export_to_fnbr()
@@ -212,7 +186,7 @@ class CCDB:
             if isinstance(part, tuple):
                 part = self.convert_link(*part, html=html)
             parts.append(part)
-        return "".join(parts) # self.clean_object(''.join(parts))
+        return "".join(parts)
 
 
     @staticmethod
@@ -247,151 +221,6 @@ class CCDB:
         if isinstance(obj, dict):
             return {key: cls.clean_object(value, html=html) for key, value in obj.items()}  # type: ignore
         return obj
-
-
-    ###########################################################################
-    ## HTML export
-
-    def print_relation_list(self, item: GlossItem, relation_type: Relation, direction: str, name: str) -> None:
-        """Prints an html version of a list of related CCs, with links to these CCs."""
-        if direction == "out":
-            relations = item.Relations.get(relation_type, [])
-        else:
-            relations = [
-                otherid
-                for otherid, other in self.glosses.items()
-                if item.Id in other.Relations.get(relation_type, ())
-            ]
-
-        if len(relations) > 0:
-            print(f'<tr><th>{name}</th> <td class="ccinfo relation">' +
-                    ' | '.join(map(self.convert_link, relations)) +
-                    '</td></tr>')
-
-
-    def print_hierarchy(self, item: GlossItem, relation_types: list[Relation], name: str) -> None:
-        parents = [(parent, rel) for rel in relation_types for parent in item.Relations.get(rel, ())]
-        children = [
-            (chid, rel)
-            for rel in relation_types
-            for chid, child in self.glosses.items()
-            if item.Id in child.Relations.get(rel, ())
-        ]
-
-        if parents or children:
-            print(f'<tr><th>{name}</th> <td class="ccinfo relation">')
-            print('<table>')
-            if parents:
-                print('<tr><td class="flex">')
-                for parent, rel in parents:
-                    pre_text = '<i>(head)</i>' if rel == Relation.HeadOf else ''
-                    print(f'<span>{pre_text} {self.convert_link(parent)}</span>')
-                print('</td></tr>')
-            print('<tr><td>')
-            print(self.convert_link(item.Id, selfid=item.Id))
-            print('</td></tr>')
-            if children:
-                print('<tr><td class="flex">')
-                for child, rel in children:
-                    pre_text = '<i>(head)</i>' if rel == Relation.HeadOf else ''
-                    print(f'<span>{pre_text} {self.convert_link(child)}</span>')
-                print('</td></tr>')
-            print('</table></td></tr>')
-
-
-    def print_html(self) -> None:
-        """Print the whole database as a single HTML file (to stdout)."""
-        print('<!DOCTYPE html>')
-        print('<html><head><meta charset="utf-8"/>')
-        print('<script src="mark.min.js"></script>')
-        print('<script src="ccdb.js"></script>')
-        print('<link rel="stylesheet" href="ccdb.css"/>')
-        print('</head><body>')
-        print(GraphSymbolSVG)
-        print('<div id="search"></div>')
-        print(f'<h1><a href="#">Database of Comparative Concepts</a></h1>')
-        print(f"<p>Extracted and expanded from the appendix of <em>Morphosyntax: Constructions of the World's Languages</em>, by William Croft (2022).")
-        print(f'In addition to this database, you can also explore our interactive <a target="CC-graph" href="cc-graph.html">graph visualization of the CC database</a>.')
-        print(f'You can also <a href="https://github.com/comparative-concepts/cc-database">look at the source code</a>,')
-        print(f'and if you have a suggestion, please <a href="https://github.com/comparative-concepts/cc-database/issues">open an issue</a>.</p>')
-        print(f'<p><strong>Database version:</strong> {self.version}</p>')
-        print(f'<p><strong>Build date/time:</strong> {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</p>')
-
-        glossitems = sorted(self.glosses.items(), key=lambda it:str.casefold(it[1].Name))
-
-        nlinks = sum(len(links) for links in self.links.values())
-        ntypedlinks = sum(len(relids) for _, item in glossitems for relids in item.Relations.values())
-        print(f'<p><strong>Statistics:</strong> {len(glossitems)} CCs, '
-              f'with {ntypedlinks} typed relations and {nlinks} links within CC definitions</p>')
-
-        print('<div id="glosses">')
-        for id, item in glossitems:
-            print(f'<div class="cc" id="{id}">')
-            print(f'<h2 class="name">{self.convert_link(id)} {self.convert_graphlink_to_html(id)}</h2>')
-            print(f'<table>')
-
-            print(f'<tr><th>Id</th> <td class="ccinfo ccid">{id}</td></tr>')
-
-            if item.Type:
-                entry = validation.GENERIC_TYPES.get(item.Type)
-                if entry:
-                    _, _, entryname = entry.partition(":")
-                    entryname = entryname.replace("-", " ")
-                    type = self.convert_link(entry, entryname)
-                elif item.Type == CCType.def_:
-                    # Just a better alias for the HTML
-                    type = 'definition'
-                else:
-                    error(id, f"Type not found: {item.Type}")
-                    type = f'<span class="notfound">{item.Type}</span>'
-                print(f'<tr><th>Type</th> <td class="ccinfo type">{type}</td></tr>')
-
-            aliases: list[str] = [item.Name] + item.Alias
-            if aliases:
-                print('<th>Alias(es)</th> <td class="ccinfo name">' +
-                      ' | '.join(map(self.convert_name, aliases)) +
-                      '</td></tr>')
-
-            self.print_relation_list(item, Relation.FunctionOf, "out", "Function of")
-            self.print_relation_list(item, Relation.FunctionOf, "in", "Function")
-            self.print_relation_list(item, Relation.ExpressionOf, "out", "Expresses")
-            self.print_relation_list(item, Relation.ExpressionOf, "in", "Expressed by")
-            self.print_relation_list(item, Relation.RecruitedFrom, "out", "Recruited from")
-            self.print_relation_list(item, Relation.RecruitedFrom, "in", "Recruited by")
-            self.print_relation_list(item, Relation.ModeledOn, "out", "Modeled on")
-            self.print_relation_list(item, Relation.ModeledOn, "in", "Modeled of")
-            self.print_relation_list(item, Relation.AttributeOf, "out", "Attribute of")
-            self.print_relation_list(item, Relation.AttributeOf, "in", "Attribute(s)")
-            self.print_relation_list(item, Relation.RoleOf, "out", "Role of")
-            self.print_relation_list(item, Relation.RoleOf, "in", "Role(s)")
-
-            self.print_hierarchy(item, [Relation.SubtypeOf], 'Taxonomy')
-            self.print_hierarchy(item, [Relation.HeadOf, Relation.ConstituentOf], 'Partonomy')
-
-            if id in self.definitions:
-                print('<tr><th>Definition</th> <td class="ccinfo definition">' +
-                      self.convert_definition(self.definitions[id]) +
-                      '</td></tr>')
-
-            if not item.FromGlossary:
-                print('''<tr><th></th><td>
-                      <span class="notfound">
-                        Not from the original glossary of <em>Morphosyntax</em>.
-                      </span></td></tr>''')
-
-            print('</table>')
-            print('</div>')
-        print('</div>')
-        print('</body></html>')
-
-
-    def convert_graphlink_to_html(self, id: str, html: bool = True) -> str:
-        """Convert a CC id into a HTML link to a graph containing just that CC."""
-        item = self.glosses[id]
-        if item.Type == CCType.def_:
-            # The definitions are not in any of the graphs, so don't return a link
-            return ""
-        return f'<a target="CC-graph" href="{GraphURL}#g={item.Type.value}&id={id}">{GraphSymbolUse}</a>'
 
 
     ###########################################################################
